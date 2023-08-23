543b5a46f524b784ba3f1498e799461256569b31
Paul K <spkim0921@gmail.com>
2023-08-22 21:44:23 -0700

    5th commit

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,13 +1,14 @@
 import { Link } from "react-router-dom"
 import {useState} from "react"
 import { MainNav } from "./MainNav"
+import capyness from "../image/capyness.png"
 
 export function Header() {
     const [showNav,setShowNav] = useState(true);
     return (
         <header>
             <div id="top-bar">
-                <a href="https://codingcapy.github.io/capytech/html/index.html"><img src="./capytech-react/capyness.png" alt="Capytech Icon" id="capytech-icon" /></a>
+                <a href="https://codingcapy.github.io/capytech/html/index.html"><img src={capyness} alt="Capytech Icon" id="capytech-icon" /></a>
                 <button id="hamburger-menu" className="hamburger-menu" onClick={()=>{
                     setShowNav(!showNav)
                 }}>&#127828;</button>
