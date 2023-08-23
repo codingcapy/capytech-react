243405ef147c54cd1dd84001bf475b882f7152cb
Paul <spkim0921@gmail.com>
2023-08-23 15:00:25 -0700

    8th commit

--- src/components/Thumbnail.js
+++ src/components/Thumbnail.js
@@ -3,10 +3,12 @@ import { Link } from "react-router-dom"
 
 export function Thumbnail(props) {
     return (
-        <div>
-            <Link to={props.path}>
+        <div className="thumbnail-container">
+            <Link to={props.path} style={{ textDecoration: 'none' }}>
                 <img src={props.src} className="thumbnail" />
-                <h2 className="video-title">{props.title}</h2>
+                <h2 className="video-meta">{props.title}</h2>
+                <p className="video-meta">{props.appUser}</p>
+                <p className="video-meta">{props.uploadDate}</p>
             </Link>
         </div>
     )
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -13,11 +13,28 @@ export function Home() {
 
     return (
         <main>
-            <h1>Capytech React Project</h1>
-            <p>You are now viewing the Capytech React website.</p>
-            <div id="videos-dashboard">
-                <Thumbnail src={streamArray.vid1.imgSrc} path={streamArray.vid1.path} title={streamArray.vid1.title} appUser={streamArray.vid1.appUser} />
-                <Thumbnail src={streamArray.vid2.imgSrc} path={streamArray.vid2.path} title={streamArray.vid2.title} appUser={streamArray.vid2.appUser} />
+            <h1 >Capytech React Project</h1>
+            <p className="main-title">You are now viewing the Capytech React website.</p>
+            <div id="main-container">
+                <div id="side-menu">
+                    <ul>
+                        <li>Home</li>
+                        <li>Shorts</li>
+                        <li>Subscriptions</li>
+                        <hr />
+                        <li>Library</li>
+                        <li>History</li>
+                        <li>Your videos</li>
+                        <li>Your movies & TV</li>
+                        <li>Watch later</li>
+                        <li>Liked videos</li>
+                        <li>Favorites</li>
+                    </ul>
+                </div>
+                <div id="videos-dashboard">
+                    <Thumbnail src={streamArray.vid1.imgSrc} path={streamArray.vid1.path} title={streamArray.vid1.title} appUser={streamArray.vid1.appUser} uploadDate={streamArray.vid1.uploadDate} />
+                    <Thumbnail src={streamArray.vid2.imgSrc} path={streamArray.vid2.path} title={streamArray.vid2.title} appUser={streamArray.vid2.appUser} uploadDate={streamArray.vid2.uploadDate} />
+                </div>
             </div>
         </main>
     )
