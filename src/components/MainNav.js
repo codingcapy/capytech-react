1b605c4f045994d6a2c5c95baab000e79180517e
Paul K <spkim0921@gmail.com>
2023-08-22 20:53:26 -0700

    setup gh-pages

--- src/components/MainNav.js
+++ src/components/MainNav.js
@@ -5,8 +5,8 @@ export function MainNav() {
     return (
         <nav id="main-nav">
             <ul id="nav-ul">
-                <li><Link to="/capytech-react" className="link">Home</Link></li>
-                <li><input placeholder="Search" /><button className="search-button">&#x1f50e;</button></li>
+                <li><Link to="/capytech-react" className="nav-link">Home</Link></li>
+                <li><div className="nav-link"><input placeholder="Search" /><button className="search-button">&#x1f50e;</button></div></li>
             </ul>
         </nav>
     )
