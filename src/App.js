21358b0c82bfc69586de925309b7493a13d85a31
Paul <spkim0921@gmail.com>
2023-08-24 15:42:38 -0700

    11th commit

--- src/App.js
+++ src/App.js
@@ -10,6 +10,11 @@ import video2 from "./videos/video-2021-12-03-23-13.mp4"
 import video3 from "./videos/video-2021-12-04-23-28-1.mp4"
 import video4 from "./videos/video-2021-12-04-23-28.mp4"
 import video5 from "./videos/video-2021-12-04-23-50-1.mp4"
+import video6 from "./videos/video-2021-12-04-23-50.mp4"
+import video7 from "./videos/video-2021-12-05-00-12.mp4"
+import video8 from "./videos/video-1-2021-12-20-221603_1.mp4"
+import video9 from "./videos/video-2-2021-12-08-231135_0.mp4"
+import video10 from "./videos/video-3-2021-12-08-213139_1.mp4"
 import { streamArray } from './pages/Home';
 
 function App() {
@@ -24,6 +29,11 @@ function App() {
           <Route path="/video-3" element={<VidPlayer src={video3} path={streamArray.vid3.path} title={streamArray.vid3.title} appUser={streamArray.vid3.appUser} uploadDate={streamArray.vid3.uploadDate} />} />
           <Route path="/video-4" element={<VidPlayer src={video4} path={streamArray.vid4.path} title={streamArray.vid4.title} appUser={streamArray.vid4.appUser} uploadDate={streamArray.vid4.uploadDate} />} />
           <Route path="/video-5" element={<VidPlayer src={video5} path={streamArray.vid5.path} title={streamArray.vid5.title} appUser={streamArray.vid5.appUser} uploadDate={streamArray.vid5.uploadDate} />} />
+          <Route path="/video-6" element={<VidPlayer src={video6} path={streamArray.vid6.path} title={streamArray.vid6.title} appUser={streamArray.vid6.appUser} uploadDate={streamArray.vid6.uploadDate} />} />
+          <Route path="/video-7" element={<VidPlayer src={video7} path={streamArray.vid7.path} title={streamArray.vid7.title} appUser={streamArray.vid7.appUser} uploadDate={streamArray.vid7.uploadDate} />} />
+          <Route path="/video-8" element={<VidPlayer src={video8} path={streamArray.vid8.path} title={streamArray.vid8.title} appUser={streamArray.vid8.appUser} uploadDate={streamArray.vid8.uploadDate} />} />
+          <Route path="/video-9" element={<VidPlayer src={video9} path={streamArray.vid9.path} title={streamArray.vid9.title} appUser={streamArray.vid9.appUser} uploadDate={streamArray.vid9.uploadDate} />} />
+          <Route path="/video-10" element={<VidPlayer src={video10} path={streamArray.vid10.path} title={streamArray.vid10.title} appUser={streamArray.vid10.appUser} uploadDate={streamArray.vid10.uploadDate} />} />
         </Routes>
       </Router>
       <Footer />
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -7,13 +7,23 @@ import vid2Thumb from "../image/video2.jpg"
 import vid3Thumb from "../image/video3.jpg"
 import vid4Thumb from "../image/video4.jpg"
 import vid5Thumb from "../image/video5.jpg"
+import vid6Thumb from "../image/video6.jpg"
+import vid7Thumb from "../image/video7.jpg"
+import vid8Thumb from "../image/video8.jpg"
+import vid9Thumb from "../image/video9.jpg"
+import vid10Thumb from "../image/video10.jpg"
 
 export let streamArray = {
     vid1: { imgSrc: vid1Thumb, path: "/video-1", title: "Wild Rift Highlights 1", appUser: "spkim0921", uploadDate: "2021-12-03" },
-    vid2: { imgSrc: vid2Thumb, path: "/video-2", title: "Wild Rift Highlights 2", appUser: "spkim0921", uploadDate: "2021-12-04" },
-    vid3: { imgSrc: vid3Thumb, path: "/video-3", title: "Wild Rift Highlights 3", appUser: "spkim0921", uploadDate: "2021-12-05" },
-    vid4: { imgSrc: vid4Thumb, path: "/video-4", title: "Wild Rift Highlights 4", appUser: "spkim0921", uploadDate: "2021-12-05" },
-    vid5: { imgSrc: vid5Thumb, path: "/video-5", title: "Wild Rift Highlights 5", appUser: "spkim0921", uploadDate: "2021-12-06" },
+    vid2: { imgSrc: vid2Thumb, path: "/video-2", title: "Wild Rift Highlights 2", appUser: "spkim0921", uploadDate: "2021-12-03" },
+    vid3: { imgSrc: vid3Thumb, path: "/video-3", title: "Wild Rift Highlights 3", appUser: "spkim0921", uploadDate: "2021-12-04" },
+    vid4: { imgSrc: vid4Thumb, path: "/video-4", title: "Wild Rift Highlights 4", appUser: "spkim0921", uploadDate: "2021-12-04" },
+    vid5: { imgSrc: vid5Thumb, path: "/video-5", title: "Wild Rift Highlights 5", appUser: "spkim0921", uploadDate: "2021-12-04" },
+    vid6: { imgSrc: vid6Thumb, path: "/video-6", title: "Wild Rift Highlights 6", appUser: "spkim0921", uploadDate: "2021-12-04" },
+    vid7: { imgSrc: vid7Thumb, path: "/video-7", title: "Wild Rift Highlights 7", appUser: "spkim0921", uploadDate: "2021-12-05" },
+    vid8: { imgSrc: vid8Thumb, path: "/video-8", title: "Wild Rift Highlights 8", appUser: "spkim0921", uploadDate: "2021-12-20" },
+    vid9: { imgSrc: vid9Thumb, path: "/video-9", title: "Wild Rift Highlights 9", appUser: "spkim0921", uploadDate: "2021-12-08" },
+    vid10: { imgSrc: vid10Thumb, path: "/video-10", title: "Wild Rift Highlights 10", appUser: "spkim0921", uploadDate: "2021-12-03" },
 }
 
 export function Home() {
@@ -25,11 +35,16 @@ export function Home() {
             <div id="main-container">
                 <SideMenu />
                 <div id="videos-dashboard">
+                    <Thumbnail src={streamArray.vid8.imgSrc} path={streamArray.vid8.path} title={streamArray.vid8.title} appUser={streamArray.vid8.appUser} uploadDate={streamArray.vid8.uploadDate} />
+                    <Thumbnail src={streamArray.vid9.imgSrc} path={streamArray.vid9.path} title={streamArray.vid9.title} appUser={streamArray.vid9.appUser} uploadDate={streamArray.vid9.uploadDate} />
+                    <Thumbnail src={streamArray.vid10.imgSrc} path={streamArray.vid10.path} title={streamArray.vid10.title} appUser={streamArray.vid10.appUser} uploadDate={streamArray.vid10.uploadDate} />
                     <Thumbnail src={streamArray.vid1.imgSrc} path={streamArray.vid1.path} title={streamArray.vid1.title} appUser={streamArray.vid1.appUser} uploadDate={streamArray.vid1.uploadDate} />
                     <Thumbnail src={streamArray.vid2.imgSrc} path={streamArray.vid2.path} title={streamArray.vid2.title} appUser={streamArray.vid2.appUser} uploadDate={streamArray.vid2.uploadDate} />
                     <Thumbnail src={streamArray.vid3.imgSrc} path={streamArray.vid3.path} title={streamArray.vid3.title} appUser={streamArray.vid3.appUser} uploadDate={streamArray.vid3.uploadDate} />
                     <Thumbnail src={streamArray.vid4.imgSrc} path={streamArray.vid4.path} title={streamArray.vid4.title} appUser={streamArray.vid4.appUser} uploadDate={streamArray.vid4.uploadDate} />
                     <Thumbnail src={streamArray.vid5.imgSrc} path={streamArray.vid5.path} title={streamArray.vid5.title} appUser={streamArray.vid5.appUser} uploadDate={streamArray.vid5.uploadDate} />
+                    <Thumbnail src={streamArray.vid6.imgSrc} path={streamArray.vid6.path} title={streamArray.vid6.title} appUser={streamArray.vid6.appUser} uploadDate={streamArray.vid6.uploadDate} />
+                    <Thumbnail src={streamArray.vid7.imgSrc} path={streamArray.vid7.path} title={streamArray.vid7.title} appUser={streamArray.vid7.appUser} uploadDate={streamArray.vid7.uploadDate} />
                 </div>
             </div>
         </main>
