a2dc744d4278edfe5195a110c8a8e1f74b7ca096
Paul K <spkim0921@gmail.com>
2023-12-17 23:05:54 -0800

    49th commit
    implemented maintenance page

---
+++ src/pages/Maintenance.js
@@ -0,0 +1,9 @@
+
+
+export default function Maintenance(){
+    return(
+        <main>
+            <h1 className="main-title" >We're sorry, CapyTV is currently under maintenance T_T Please check back on December 20, 2023!</h1>
+        </main>
+    )
+}
--- src/router.js
+++ src/router.js
@@ -13,17 +13,21 @@ import { VidPlayer, vidPlayerLoader } from './pages/VidPlayer';
 import LoginPage from './pages/Login';
 import SignupPage from './pages/Signup';
 import ProfilePage, { profileLoader } from "./pages/Profile";
+import Maintenance from "./pages/Maintenance";
 
 export function Router() {
 
     const router = createBrowserRouter(
         createRoutesFromElements(
             <Route element={<Layout />}>
-                <Route path="/capytech-react" element={<Home />} loader={pageLoader} />
-                <Route path="/capytech-react/login" element={<LoginPage />} />
-                <Route path="/capytech-react/signup" element={<SignupPage />} />
+                <Route path="/capytech-react/maintenance" element={<Home />} loader={pageLoader} />
+                <Route path="/capytech-react/maintenance2" element={<LoginPage />} />
+                <Route path="/capytech-react/maintenance3" element={<SignupPage />} />
                 <Route path="/capytech-react/videos/:videoId" element={<VidPlayer />} loader={vidPlayerLoader} />
                 <Route path="/capytech-react/users/:userId" element={<ProfilePage />} loader={profileLoader} />
+                <Route path="/capytech-react/" element={<Maintenance />} />
+                <Route path="/capytech-react/login" element={<Maintenance />} />
+                <Route path="/capytech-react/signup" element={<Maintenance />} />
             </Route>
         )
     )
