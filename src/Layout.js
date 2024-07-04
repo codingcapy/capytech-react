6f85c43a92b78f70829b0d2941e0ddfa004782a6
Paul K <spkim0921@gmail.com>
2024-07-04 16:57:32 -0700

    53rd commit
    built and implemented scroll to top component

--- src/Layout.js
+++ src/Layout.js
@@ -9,10 +9,12 @@ description: layout js for CapyTV
 import { Outlet } from "react-router-dom";
 import { Header } from "./components/Header";
 import { Footer } from "./components/Footer";
+import { ScrollToTop } from "./ScrollToTop";
 
 export default function Layout() {
     return (
         <div id='wrapper'>
+            <ScrollToTop />
             <Header />
             <main>
                 <Outlet />
